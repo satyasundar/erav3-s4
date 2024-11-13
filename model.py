22d665d521c6ae809f1ea6e7e1738c29eeb33080
satyasundar <satyasundar.nayak@gmail.com>
2024-11-13 21:14:41 +0530

    model comparision with custom number of filteres for each layer v4

--- model.py
+++ model.py
@@ -10,17 +10,24 @@ class MNISTNet(nn.Module):
         # Get activation function
         self.activation = self._get_activation()
         
-        # Define layers
-        self.conv1 = nn.Conv2d(1, config['num_filters'], kernel_size=config['kernel_size'], padding=config['kernel_size']//2)
-        self.conv2 = nn.Conv2d(config['num_filters'], config['num_filters']*2, kernel_size=config['kernel_size'], padding=config['kernel_size']//2)
-        self.conv3 = nn.Conv2d(config['num_filters']*2, config['num_filters']*2, kernel_size=config['kernel_size'], padding=config['kernel_size']//2)
-        self.conv4 = nn.Conv2d(config['num_filters']*2, config['num_filters']*4, kernel_size=config['kernel_size'], padding=config['kernel_size']//2)
+        # Define layers with individual filter counts
+        self.conv1 = nn.Conv2d(1, config['num_filters_1'], kernel_size=config['kernel_size'], 
+                              padding=config['kernel_size']//2)
+        self.conv2 = nn.Conv2d(config['num_filters_1'], config['num_filters_2'], 
+                              kernel_size=config['kernel_size'], padding=config['kernel_size']//2)
+        self.conv3 = nn.Conv2d(config['num_filters_2'], config['num_filters_3'], 
+                              kernel_size=config['kernel_size'], padding=config['kernel_size']//2)
         
         # Set pooling layer
         self.pool = nn.MaxPool2d(2, 2) if config['pooling'] == 'max' else nn.AvgPool2d(2, 2)
         
+        # Calculate the size of flattened features after convolutions and pooling
+        # Input image is 28x28, after 3 pooling layers it becomes 3x3
+        # So the flattened size will be: num_filters_3 * 3 * 3
+        self.flat_features = config['num_filters_3'] * 3 * 3
+        
         self.dropout = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(config['num_filters']*4, 10)
+        self.fc1 = nn.Linear(self.flat_features, 10)
         
     def _get_activation(self):
         if self.config['activation'] == 'relu':
@@ -33,12 +40,11 @@ class MNISTNet(nn.Module):
     def forward(self, x):
         batch_size = x.size(0)
         
-        x = self.pool(self.activation(self.conv1(x)))
-        x = self.pool(self.activation(self.conv2(x)))
-        x = self.pool(self.activation(self.conv3(x)))
-        x = self.pool(self.activation(self.conv4(x)))
+        x = self.pool(self.activation(self.conv1(x)))  # 28x28 -> 14x14
+        x = self.pool(self.activation(self.conv2(x)))  # 14x14 -> 7x7
+        x = self.pool(self.activation(self.conv3(x)))  # 7x7 -> 3x3
         
-        x = x.view(batch_size, -1)
+        x = x.view(batch_size, -1)  # Flatten: (batch_size, num_filters_3 * 3 * 3)
         x = self.dropout(x)
         x = self.fc1(x)
         return x
